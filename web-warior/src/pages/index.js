b9f97e3ee14be841fbabfdecfeee217da53bdc07
moise <mose.ns@hotmail.com>
2021-03-01 13:01:04 +0100

    styles and static file

--- web-warior/src/pages/index.js
+++ web-warior/src/pages/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Layout from '../components/Layout';
-
-
+import styles from '../styles/home.module.css';
+import { Link } from 'gatsby';
 
 export default function Home() {
   return(
     <Layout>
-      <section>
+      <section className={styles.header}>
       <div>
         <h2>Design</h2>
         <h3>Develop & Deploy</h3>
         <p>Ux designer & web developer based in Brusssels</p>
+        <Link className={styles.btn} to='/projects'>My Portfolio Projects</Link>
       </div>
+      <img src="/banner.png" alt="site banner" style={{maxWidth: '100%'}}/>
     </section>
     </Layout>
   )
--- web-warior/src/pages/projects/index.js
+++ web-warior/src/pages/projects/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Layout from '../../components/Layout';
+import styles from '../../styles/projects.module.css';
+
 
 export default function Project() {
   return (
     <Layout>
-      <div>
+      <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
         <p>Ux designer & web developer based in Brusssels</p>
