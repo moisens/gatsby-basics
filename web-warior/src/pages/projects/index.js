03ac5d4d0413f295df2cb657f06cd25339b74edd
moise <mose.ns@hotmail.com>
2021-04-16 17:16:16 +0200

    Fetching & Listing data

--- web-warior/src/pages/projects/index.js
+++ web-warior/src/pages/projects/index.js
@@ -1,16 +1,43 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import styles from '../../styles/projects.module.css';
+import { graphql, Link } from 'gatsby';
 
-
-export default function Project() {
+export default function Project({ data }) {
+  console.log(data);
+  const projects = data.allMarkdownRemark.nodes;
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
-        <p>Ux designer & web developer based in Brusssels</p>
+        <div className={styles.projects}>
+          {projects.map(project => (
+            <Link to={`/projects ${project.frontmatter.slug}`} key={`${project.id}`}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+//export page query
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`; 
