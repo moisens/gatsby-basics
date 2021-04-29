d538a677eb8a609c17db4b9de05c019d669c67e4
moise <mose.ns@hotmail.com>
2021-04-29 18:36:31 +0200

    optimized image

--- web-warior/gatsby-config.js
+++ web-warior/gatsby-config.js
@@ -8,11 +8,21 @@ module.exports = {
   /* Your site config here */
   plugins: [
     'gatsby-transformer-remark',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `projects`,
         path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
   ],
--- web-warior/src/pages/index.js
+++ web-warior/src/pages/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Layout from '../components/Layout';
 import styles from '../styles/home.module.css';
-import { Link } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+// import Img from "gatsby-image"
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
-export default function Home() {
+export default function Home({ data }) {
+  const image = getImage(data.file.childrenImageSharp[0])
+  //console.log(image);
   return(
     <Layout>
       <section className={styles.header}>
@@ -13,10 +17,19 @@ export default function Home() {
         <p>Ux designer & web developer based in Brusssels</p>
         <Link className={styles.btn} to='/projects'>My Portfolio Projects</Link>
       </div>
-      <img src="/banner.png" alt="site banner" style={{maxWidth: '100%'}}/>
-      
+      <GatsbyImage image={image} alt="header-image" />
     </section>
     </Layout>
   )
 }
 
+export const query = graphql `
+  query Banner {
+    file(relativePath: {eq: "banner.png"}) {
+      childrenImageSharp {
+        gatsbyImageData(layout: FULL_WIDTH)
+      }
+    }
+  }
+
+`;
